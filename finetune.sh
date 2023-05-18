7f68e88487a2a00fb5d42ff01f9fd1959b205730
江湖十年 <jianghushinian007@163.com>
2023-05-18 14:54:54 +0800

    add sh

---
+++ finetune.sh
@@ -0,0 +1,18 @@
+TOT_CUDA="0,1"
+CUDAs=(${TOT_CUDA//,/ })
+CUDA_NUM=${#CUDAs[@]}
+PORT="12345"
+
+DATA_PATH="/kaggle/working/test-data/couplet-10k.json" #"../dataset/instruction/guanaco_non_chat_mini_52K-utf8.json" #"./sample/merge_sample.json"
+OUTPUT_PATH="lora-Vicuna"
+MODEL_PATH="decapoda-research/llama-7b-hf"
+lora_checkpoint="./lora-Vicuna/checkpoint-11600"
+TEST_SIZE=200
+
+CUDA_VISIBLE_DEVICES=${TOT_CUDA} torchrun --nproc_per_node=$CUDA_NUM --master_port=$PORT finetune.py \
+--data_path $DATA_PATH \
+--output_path $OUTPUT_PATH \
+--model_path $MODEL_PATH \
+--eval_steps 200 \
+--save_steps 200 \
+--test_size $TEST_SIZE
